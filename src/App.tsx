bc631b1f8b666036842f99ac3f9f6a0a729d0ef3
Margub Murshed <murshedmargub@gmail.com>
2025-09-07 18:35:51 +0600

    Project initialization with redux setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      <Button>This is button</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const envVariables = {
+    baseURL: import.meta.env.VITE_BASE_URL
+}
