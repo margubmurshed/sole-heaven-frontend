110c17dffb005f8d3e7364ed6c8fcb3993168f48
Margub Murshed <murshedmargub@gmail.com>
2025-09-08 02:41:12 +0600

    Create Responsive Navbar

--- src/components/layouts/PrimaryLayout.tsx
+++ src/components/layouts/PrimaryLayout.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { Outlet } from 'react-router';
+import { Navbar } from './Navbar';
 
 const PrimaryLayout = () => {
     return (
         <div>
-            
+            <Navbar />
+            <Outlet />
         </div>
     );
 };
